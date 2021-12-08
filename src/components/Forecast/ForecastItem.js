e2c719e2131ef290244528be0cf005e0d7ca4380
Abhinav Sawarkar <abhisawarkar85@gmail.com>
2021-12-08 18:16:45 +0530

    Updated Forecast Component

--- src/components/Forecast/ForecastItem.js
+++ src/components/Forecast/ForecastItem.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const ForecastItem = ({ item }) => {
   const forecastIcon = `http://openweathermap.org/img/wn/${item.weather[0].icon}@4x.png`;
+
   return (
     <li className=" border-2 border-transparent flex flex-col justify-evenly bg-dark-blue focus:border-white">
+      <p className="text-center font-semibold text-lg">{item.day}</p>
       <img src={forecastIcon} alt="forecast icon" />
       <div className="flex justify-between items-center px-3 font-semibold text-md">
         <span>{item.temp.max.toFixed(1)}&#176;C</span>
--- src/components/Forecast/ForecastList.js
+++ src/components/Forecast/ForecastList.js
@@ -1,23 +1,33 @@
 import React, { useContext } from "react";
 import WeatherContext from "../../store/weather-context";
 import ForecastItem from "./ForecastItem";
+import moment from "moment";
 
 const ForecastList = () => {
-  const { daily } = useContext(WeatherContext);
+  const weatherCtx = useContext(WeatherContext);
+  console.log(weatherCtx);
+
   const forecastDaily =
-    daily &&
-    daily.map((item) => {
+    weatherCtx?.daily &&
+    weatherCtx.daily.map((item, index) => {
       return {
+        day: moment()
+          .add(index + 1, "days")
+          .format("dddd"),
         temp: item.temp,
         weather: item.weather,
       };
     });
+  console.log(forecastDaily);
   return (
     <ul className="w-full h-screen bg-very-dark-blue text-white grid px-9 py-12  grid-cols-2 gap-x-6 gap-y-8">
       {forecastDaily &&
         forecastDaily.map((item, index) => (
           <ForecastItem item={item} key={index} />
         ))}
+      {!weatherCtx && !forecastDaily && (
+        <p className="text-center">No Context Found</p>
+      )}
     </ul>
   );
 };
